ede915f1f13ee286f3365350a4cd6b287b66fcf1
GirishNA-12 <girishadiram@gmail.com>
2025-02-25 22:34:55 +0530

    Delete model.py

--- model.py
+++
@@ -1,6 +0,0 @@
-from transformers import QuestionAnsweringPipeline, pipeline
-
-def question_answer(context: str, question: str):
-    qa_pipeline = pipeline("question-answering")
-    result = qa_pipeline(question=question, context=context)
-    return result['answer']
